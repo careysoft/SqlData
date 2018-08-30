afef56a5d38d3de784f5b125eb9c5a3837805426
careysoft <37427795+careysoft@users.noreply.github.com>
2018-08-30 11:15:27 +0800

    2018083001

---
+++ Careysoft.Dotnet.Tools.SqlData.ManageClient/Careysoft.Dotnet.Tools.SqlData.ManageClient/FormMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Careysoft.Dotnet.Tools.SqlData.ManageClient
+{
+    public partial class FormMain : DevExpress.XtraEditors.XtraForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
